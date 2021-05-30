636778f24d8654033a06b7942f611e085ff8259a
Nachiappan <nachiapan@gmail.com>
2021-05-30 15:59:42 +0530

    Completed output writing

---
+++ AspNetCoreDemo/AspNetCoreDemo/Controllers/OutputController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.Controllers
+{
+
+    [ApiController]
+    [Route("Output")]
+    public class OutputController : ControllerBase
+    {
+        [HttpGet("write-header")]
+        public string AddHeader()
+        {
+            HttpContext.Response.Headers.Add("header-name", "header-value");
+            return "result";
+        }
+
+
+        [HttpGet("set-status-code")]
+        public string ChangeStatusCode()
+        {
+            HttpContext.Response.StatusCode = 207;
+            return "result";
+        }
+
+
+        [HttpGet("set-status-code1")]
+        public ActionResult<string> ChangeStatusCode1()
+        {
+            return StatusCode(209, "result");
+        }
+
+
+        [HttpGet("write-body")]
+        public void WriteBody()
+        {
+            byte[] data = Encoding.UTF8.GetBytes("output");
+            Response.Body.WriteAsync(data, 0, data.Length).Wait();
+        }
+    }
+}
