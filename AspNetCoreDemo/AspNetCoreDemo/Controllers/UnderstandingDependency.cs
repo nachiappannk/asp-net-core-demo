3ea0c43133bc3127a7bb091c1239e81b221b8472
Nachiappan <nachiapan@gmail.com>
2021-05-30 14:12:58 +0530

    Intial version

---
+++ AspNetCoreDemo/AspNetCoreDemo/Controllers/UnderstandingDependency.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.Controllers
+{
+    [ApiController]
+    [Route("ReadingDataFromRequest")]
+    public class UnderstandingDependencyController : ControllerBase
+    {
+        public UnderstandingDependencyController()
+        {
+
+        }
+
+
+        [HttpGet("some-method")]
+        public string ReadHeader()
+        {
+            return "Some string";
+        }
+    }
+
+
+
+    public class ObjectOne
+    {
+        public ObjectOne(ObjectTwo objectTwo)
+        {
+            ObjectTwo = objectTwo;
+        }
+
+        public ObjectTwo ObjectTwo { get; }
+    }
+
+
+    public class ObjectTwo
+    {
+
+    }
+
+}
